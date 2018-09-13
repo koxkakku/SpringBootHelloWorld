c40ff1dcbe87a533d4ae0efbb59ffbcd155f467b
koxkakku <sharad.k.dutta@gmail.com>
2018-09-13 23:02:07 +0530

    modified urlconstants

---
+++ src/main/java/com/kox/springboot/constants/UrlConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.kox.springboot.constants;
+
+/**
+ * @author KOXKAKKU
+ *
+ */
+public class UrlConstants {
+
+	public static final String URL_GREETING = "/greeting";
+
+}
